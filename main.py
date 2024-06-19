8b540d4903e22d15e7feab111f4e7cc7bc5543dd
Luis-E-Ortega <lorte030@gmail.com>
2024-06-19 15:56:39 -0400

    Final changes, added print report function

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ def main():
     text = open_and_read(book_path)
     num_words = word_counter(text)
     letter_frequency_count = character_counter(text)
+    printable = print_report(book_path, text, letter_frequency_count)
 
 def open_and_read(path):
     with open(path) as f:
@@ -25,9 +26,22 @@ def character_counter(text):
                 char_counter[c] += 1 #Increment the count value for the existing letter key
             else:
                 char_counter[c] = 1 #Initialize the key/value pair with the letter and 1
-    sorted_char_counter = dict(sorted(char_counter.items()))
+    #sorted_char_counter = dict(sorted(char_counter.items()))
+    sorted_char_counter = sorted(char_counter.items(), key=lambda item: item[1], reverse=True)
     print(sorted_char_counter)
-    return char_counter 
+    return sorted_char_counter
+
+def print_report(path, text, dictionary):
+    words = text.split()
+    count = len(words)
+
+    print(f"--- Begin report of {path} ---\n{count} words found in the document\n\n")
+    
+    for char, count in dictionary:
+        print(f"The '{char}' character was found {count} times")
+    print("--- End report ---")
+
+
 
 if __name__ == "__main__":
     main()
